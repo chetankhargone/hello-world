16b2c34db6bc06f9e5f202e2389de2baf6887457
Chetan Gupta <chetan.gupta21@gmail.com>
2015-06-17 00:46:12 +0530

    Intial committ to github

---
+++ Angular-ui-routing-demo/Content/Angular/app.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="angular.min.js" />
+var app = angular.module('demoApp', ['ui.router'])
+
+
+
+app.config(['$routeProvider','$location'], function($routeProvider)
+{
+    when('/',
+        {
+            templateUrl: '/AlbumsList.html',
+            controller: 'a1Ctrl'
+        }).
+    when('/albums/:albumName',
+    {
+        templateUrl: 'AlbumsList.html',
+        controller: 'b1Ctrl'
+    })
+})
+app.controller('a1Ctrl', function($scope,$http){
+
+    $scope.albums = function()
+    {
+        //ajax getting data from server 
+    }
+})
+
+app.controller('b1Ctrl', function ($scope, $routeProviders) {
+    $scope.album = $routeProviders.albumName;
+
+    $http({
+        //same ajax function as above getting data from server for the nes sub album
+    })
+})
+
+app.controller()
---
+++ Angular-ui-routing-demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Angular_ui_routing_demo.MvcApplication" Language="C#" %>
